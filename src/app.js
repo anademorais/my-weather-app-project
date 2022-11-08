2965595d09db2cec82113d65f85da2fba486f5f8
anademorais <116738781+anademorais@users.noreply.github.com>
2022-11-08 12:27:28 +0000

    Inside the Daily Forecast I updated the coordinates to the API from openweather one call

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ function formatDate(timestamp) {
 	return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+	console.log(response.data);
 	let forecastElement = document.querySelector("#forecast");
 	let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
 	let forecastHTML = `<div class="row">`;
@@ -47,6 +48,13 @@ function displayForecast() {
 	forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+	console.log(coordinates);
+	let apiKey = "4c9b53e4f8f5eb00df5915bdca340605";
+	let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+	axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
 	let temperatureElement = document.querySelector("#temperature");
 	let cityElement = document.querySelector("#city");
@@ -69,6 +77,8 @@ function displayTemperature(response) {
 		`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
 	);
 	iconElement.setAttribute("alt", response.data.weather[0].description);
+
+	getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -111,4 +121,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Lisbon");
-displayForecast();
